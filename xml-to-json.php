48aa6453edea85abba97b9e78b223d3f67051a9e
Gino <ginocote@gmail.com>
2017-01-28 23:47:37 -0500

    First upload file

---
+++ xml-to-json.php
@@ -0,0 +1,26 @@
+<?php
+header('Content-Type: application/json; charset=utf-8');
+// Obtain Feed Url From POST
+$feed_url = $_GET['url'];
+
+if(!$_GET['url'])
+$feed_url = 'http://news.yahoo.com/rss';
+
+$xml = @simplexml_load_file($feed_url, 'SimpleXMLElement', LIBXML_NOCDATA);
+
+$json = json_encode($xml);
+// jsonP callback
+//$json = $_GET['callback']. '(' . json_encode($xml) . ');';
+
+echo $json;
+
+/*
+
+jQuery Example
+
+$.getJSON('xml-to-json.php', function(data) { 
+	//do something
+});
+
+*/
+?>
